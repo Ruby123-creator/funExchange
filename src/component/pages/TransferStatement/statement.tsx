44589215f2ed1bacf74f31f0de520ea1225fab3e
Ruby123-creator <rubyp20001@gmail.com>
2025-03-11 13:19:02 +0530

    first commit

--- src/component/pages/TransferStatement/statement.tsx
+++ src/component/pages/TransferStatement/statement.tsx
@@ -35,11 +35,11 @@ const Statements : React.FC<Props> = ({data}) => {
                     className="border-b border-gray-200 hover:bg-gray-50 text-xs"
                   >
                     <td className="p-2">{bet.index}</td>
-                    <td className=" p-2">{bet.credit}</td>
+                    <td className=" p-2 text-text_Profit">₹{bet.credit}</td>
                     
                    
-                    <td className="p-2">{bet.debit}</td>
-                    <td className="p-2">₹ {bet.balance}</td>
+                    <td className="p-2 text-text_Danger">₹{bet.debit}</td>
+                    <td className="p-2">₹{bet.balance}</td>
                    
                     <td className="p-2"> {bet.time}</td>
                    
