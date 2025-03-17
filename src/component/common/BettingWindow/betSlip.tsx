aa06e5580e01eacdd679d3b26af84e655d851e03
Ruby123-creator <rubyp20001@gmail.com>
2025-03-17 12:00:03 +0530

    first commit

--- src/component/common/BettingWindow/betSlip.tsx
+++ src/component/common/BettingWindow/betSlip.tsx
@@ -137,7 +137,7 @@ const BetSlip: React.FC = () => {
     else{
       if (
         (betOdds.type === "lay" && currentOdds > betOddsValue) ||
-        (betOdds.type === "back" && currentOdds < betOddsValue) || (checkCurrentBet?.status === "SUSPENDED")
+        (betOdds.type === "back" && currentOdds < betOddsValue) || (checkCurrentBet?.status === "SUSPENDED" || (checkCurrentBet?.status === "Ball Running"))
       ) {
         showToasterMessage({ messageType: "error", description: "Odds invalid" });
         return false;
