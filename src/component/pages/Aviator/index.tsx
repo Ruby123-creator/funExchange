684d09a6bde337913b1f95f7ca36e9bcc7d399a7
Ruby123-creator <rubyp20001@gmail.com>
2025-04-03 11:48:08 +0530

    first-commit

--- src/component/pages/Aviator/index.tsx
+++ src/component/pages/Aviator/index.tsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 const AviatorComp: React.FC = () => {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+  useEffect(() => {
+      const sendAuthToken = () => {
+          const token = localStorage.getItem("authToken"); // Get the auth token
+
+          if (token && iframeRef.current) {
+              iframeRef.current.contentWindow?.postMessage({ token }, "https://aviator-flame.vercel.app/");
+          }
+      };
+
+      // Send the token once the iframe loads
+      if (iframeRef.current) {
+          iframeRef.current.onload = sendAuthToken;
+      }
+  }, []);
   return (
     <div className="flex flex-col  transition-all lg:pt-[110px] ease-in-out duration-100 pt-0">
     <div className="flex items-start justify-start w-full ">
         <div className="w-full md:mt-[0px]   lg:overflow-auto" style={{minHeight: "calc(-110px + 100dvh)"}}>
             <div></div>
             <div className="bg-transparent w-full h-full">
-                <div className="  w-full flex h-[calc(100dvh-42px)] lg:h-[calc(100dvh-54px)]"><iframe
-                        src="https://live.fantasylineups.com/?host_id=diamond222&amp;token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTU1Y2UwNTcxZGU1OTExNjIyNGNhNDkiLCJpYXQiOjE3MzQ2OTUzMDIsImV4cCI6MTczNTMwMDEwMn0.yEFTrajM-k30b9eQFrswxyrN_Njv3CU4Z3YzPrSK9F4"
-                        className="w-full h-full flex-grow no-scrollbar" allowFullScreen={true}
-                        aria-hidden="true"></iframe></div>
+                <div className="  w-full flex h-[calc(100dvh-42px)] lg:h-[calc(100dvh-54px)]">
+                <iframe
+                ref={iframeRef}
+                id="iframe-id"
+                src="https://aviator-flame.vercel.app/"
+                width="800"
+                height="600"
+                title="Second Website"
+                className="w-full h-full flex-grow no-scrollbar" allowFullScreen={true}
+                        aria-hidden="true"
+            ></iframe>
+                 </div>
             </div>
         </div>
     </div>
